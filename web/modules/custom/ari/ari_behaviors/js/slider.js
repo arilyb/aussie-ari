96c50542741bba33d7402d5d2317c6b9a82b1859
Ari Lybaert <ari_lybaert@outlook.com>
2024-09-12 18:22:24 +0800

    worked on various bugs and did some theming

--- web/modules/custom/ari/ari_behaviors/js/slider.js
+++ web/modules/custom/ari/ari_behaviors/js/slider.js
@@ -1,5 +1,19 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3.2,
-    spaceBetween: 20,
+    slidesPerView: 1.2,
+    spaceBetween: 10,
     freeMode: true,
+
+    breakpoints: {
+        575: {
+          slidesPerView: 1.3,
+          spaceBetween: 10,
+        },
+        600: {
+          slidesPerView: 2.4,
+          spaceBetween: 15,
+        },
+        768: {
+          slidesPerView: 3.4,
+          spaceBetween: 20,
+        }}
   });
